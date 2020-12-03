682b4411bcddcc8ae882c29f51286370d7ef9ed3
sarah-svg <kjskillen@gmail.com>
2020-12-02 16:28:56 -0800

    promise all

--- sandbox/rickandmorty.js
+++ sandbox/rickandmorty.js
@@ -1,10 +1,16 @@
 const fetch = require('node-fetch');
 
 
+// fetch('https://rickandmortyapi.com/api/character')
+//     .then(res => {
+//         return res.json();
+//     })
+//     .then(json => {
+//         console.log(json);
+//     })
+Promise.all([
 fetch('https://rickandmortyapi.com/api/character')
-    .then(res => {
-        return res.json();
-    })
-    .then(json => {
-        console.log(json);
-    })
+])
+.then(name => {
+           console.log(name);
+      });
