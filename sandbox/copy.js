183ea5d942d0a79081e31cdb1dc8348d0dc2075d
sarah-svg <kjskillen@gmail.com>
2020-12-02 20:38:56 -0800

    copy in sandbox

--- sandbox/copy.js
+++ sandbox/copy.js
@@ -1,8 +1,29 @@
-const fs = require('fs').promises;
+//copyfile.js
+const fs = require('fs');
+const { fileURLToPath } = require('url');
 
-const copy = () => {
-    return fs.copyFile(
-      
+// destination will be created or overwritten by default.
 
-    )
-}
+fs.copyFile('sandbox/read2.txt',
+ 'sandbox/copy.txt',
+  (err) => {
+  if (err) throw err;
+  console.log('File was copied to destination copy1');
+
+  })
+
+  //////////
+  const copyFileAsync = async () => {
+      try {
+        const newFile = await fs.copyFile('sandbox/read2.txt',
+        'sandbox/copy2.txt',
+         (err) => {
+         if (err) throw err;
+      }
+        );
+        return file;
+  } catch (err) {
+      console.log('File was copied to destination copy2')
+  }};
+
+copyFileAsync();
